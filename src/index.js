8839760dd6eb779eeb132a991ec9d52a9b49aa21
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-08-23 15:52:16 +0200

    fix clamp function

--- src/index.js
+++ src/index.js
@@ -216,12 +216,12 @@ export const getPos = (el) => {
  * @param {Number|null} min - Lower edge of the desired range
  * @param {Number|null} max - Upper edge of the desired range
  */
-export const clamp = (value, min = null, max = null) => {
-  if (min && max) {
+export const clamp = (value, min, max) => {
+  if (min !== undefined && max !== undefined) {
     return Math.min(Math.max(value, min), max)
-  } else if (min) {
+  } else if (min !== undefined) {
     return Math.max(value, min)
-  } else if (max) {
+  } else if (max !== undefined) {
     return Math.min(value, max)
   } else {
     return value
