ebec4c2c46c9012d029b8e4020557860cd6d30dd
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-10-08 12:11:02 +0200

    delete umd folder before build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ import { terser } from 'rollup-plugin-terser'
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import rimraf from 'rimraf'
+import path from 'path'
+
+rimraf.sync(path.resolve(__dirname, 'umd'))
 
 export default [
   // normal
