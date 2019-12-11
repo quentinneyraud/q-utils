feadc43e3f46b801a529f2479ee923480b69b7f5
Quentin Neyraud <quentin.neyraud@gmail.com>
2019-12-11 13:12:43 +0100

    Fix functions export

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-export const addClass = require('./functions/addClass').default
-export const addEvent = require('./functions/addEvent').default
-export const clamp = require('./functions/clamp').default
-export const debounce = require('./functions/debounce').default
-export const gebc = require('./functions/gebc').default
-export const gebi = require('./functions/gebi').default
-export const getPos = require('./functions/getPos').default
-export const hasProperties = require('./functions/hasProperties').default
-export const isOneOf = require('./functions/isOneOf').default
-export const lerp = require('./functions/lerp').default
-export const logElement = require('./functions/logElement').default
-export const map = require('./functions/map').default
-export const qs = require('./functions/qs').default
-export const qsa = require('./functions/qsa').default
-export const removeClass = require('./functions/removeClass').default
-export const removeEvent = require('./functions/removeEvent').default
-export const serializeForm = require('./functions/serializeForm').default
-export const setStyle = require('./functions/setStyle').default
+import addClass from './functions/addClass'
+import addEvent from './functions/addEvent'
+import clamp from './functions/clamp'
+import debounce from './functions/debounce'
+import gebc from './functions/gebc'
+import gebi from './functions/gebi'
+import getPos from './functions/getPos'
+import hasProperties from './functions/hasProperties'
+import isOneOf from './functions/isOneOf'
+import lerp from './functions/lerp'
+import logElement from './functions/logElement'
+import map from './functions/map'
+import qs from './functions/qs'
+import qsa from './functions/qsa'
+import removeClass from './functions/removeClass'
+import removeEvent from './functions/removeEvent'
+import serializeForm from './functions/serializeForm'
+import setStyle from './functions/setStyle'
 
 export default {
   addClass,
